63dfe2835b3726f9077e8c844b095dba62dd850e
Maksym Kramarenko <maksym@wix.com>
2019-05-17 21:22:07 +0300

    Add map

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import RestaurantList from "./components/restaurant-list";
 import UserForm from "./components/user-form";
+import RestaurantsMap from "./components/restaurants-map";
 
 function App(props) {
   return (
     <div className="App">
       <RestaurantList restaurants={props.restaurants} />
+      <RestaurantsMap restaurants={props.restaurants} />
       <UserForm />
     </div>
   );
