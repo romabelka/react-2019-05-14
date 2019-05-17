7980fb872794de7c46dc2c9c9bf53f59def9bbac
Maksym Kramarenko <maksym@wix.com>
2019-05-17 20:57:03 +0300

    Hooks

--- src/components/dish.js
+++ src/components/dish.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Dish(props) {
+  const [amount, decrease, increase] = useCounter(0);
   return (
     <div>
       <span>{props.name}</span>
       <span style={{ float: "right" }}>{props.price}</span>
+      <hr />
+      <span>{amount}</span>
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
     </div>
   );
+}
+
+function useCounter(initialValue) {
+  const [value, setAmount] = useState(initialValue);
+  return [
+    value,
+    () => setAmount(value <= 0 ? 0 : value - 1),
+    () => setAmount(value + 1)
+  ];
 }
 
 export default Dish;
