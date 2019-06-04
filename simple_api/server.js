0965d4056b829f40a722b8bfe04c2344a654d11b
Maksym Kramarenko <maksym@wix.com>
2019-06-04 21:27:36 +0300

    Update server header

--- simple_api/server.js
+++ simple_api/server.js
@@ -8,10 +8,6 @@ var app = express();
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
   next();
 });
 app.use(bodyParser.json());
